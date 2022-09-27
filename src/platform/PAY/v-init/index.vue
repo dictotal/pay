00dcf255dcd1a10014f7480a60ea09fb1f59a89b
Neil <neil@gmail.com>
2022-09-27 12:18:31 +0800

    描述文案最多显示2行改为3行

--- src/platform/PAY/v-init/index.vue
+++ src/platform/PAY/v-init/index.vue
@@ -436,7 +436,7 @@ export default {
       word-break: break-all;  /* break-all(允许在单词内换行。) */
       display: -webkit-box; /* 对象作为伸缩盒子模型显示 */
       -webkit-box-orient: vertical; /* 设置或检索伸缩盒对象的子元素的排列方式 */
-      -webkit-line-clamp: 2; /* 显示的行数 */
+      -webkit-line-clamp: 3; /* 显示的行数 */
     }
     .bank-child-arrow {
       margin-left: 10px;
