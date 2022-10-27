7b60e636fa97c076fccea53dd45c0ea980bbaf87
Neil <neil@gmail.com>
2022-10-27 14:49:52 +0800

    更换版本

--- src/common/utils/string-utils.js
+++
@@ -1,9 +0,0 @@
-let StringUtils = {
-	format(str, repArg) {
-		return str.replace(/\{([\da-zA-Z]+)\}/g, function () {
-			return repArg[arguments[1]] ? repArg[arguments[1]] : arguments[0];
-		});
-	}
-};
-
-export default StringUtils;
--- src/config/index.js
+++
@@ -1,3 +0,0 @@
-export default {
-	isTestModel: process.env.NODE_ENV === "development"
-};
